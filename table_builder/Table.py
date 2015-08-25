cda7c556f66045ad6caa1ce34bd9065af23a4dd6
Amir Bar <amirbar@emerald.hanah>
2015-08-25 17:00:04 +0300

    added function to match directionality

--- table_builder/Table.py
+++ table_builder/Table.py
@@ -8,6 +8,10 @@ class Table(object):
     """
 
     UNIQUE_ID_FIELD = "unique_id"
+    NAME_FIELD = "NAME"
+    OTHER_NAMES_FIELD = "other_names"
+    POS_STRAND = "+"
+    NEG_STRAND = "-"
 
     def __init__(self, name):
         """
@@ -19,12 +23,11 @@ class Table(object):
         self._name = name
         self._dctData = {}
 
-    def insert(self, start, end, strand, **kwargs):
+    def insert(self, first, second, **kwargs):
         """
         Insert a new entry to the table
-        :param start: the start position
-        :param end: the end position
-        :param strand: the strand string (e.g "positive" or "negative")
+        :param first: a tuple (start, end, strand)
+        :param second: a tuple (start, end, strand)
         :param kwargs: a dictionary containing additional info
         :return: None
         """
@@ -32,73 +35,132 @@ class Table(object):
         additional_data.update(kwargs)
         additional_data[Table.UNIQUE_ID_FIELD] = len(self._dctData)
 
-        self._dctData["%s;%s;%s" % (str(start), str(end), strand)] = additional_data
+        id = ("%s;%s;%s;" % first) + ("%s;%s;%s" % second)
 
-    def is_overlaps(self, start, end, strand):
+        if (self._dctData.has_key(id)):
+            print "[Table:insert]warning multiple entries has the unique id: %s" % id
+
+        self._dctData[id] = additional_data
+
+    def is_overlaps(self, start, end, strand_val):
         """
         Checks whether a given segment overlaps with
         any of the entries in the table
         :param start: the start position
         :param end: the end position
-        :param strand: the strand string (e.g "positive" or "negative")
+        :param strand_val: the strand string (e.g "positive" or "negative")
         :return: a tuple containing True or False whether it has overlapping segment
                  and the id of the closest segment found (None if there isn't one)
         """
         closest_segment = None
         closest_segment_distance = None
 
+        matches = []
+
         for key, value in self._dctData.items():
-            entry_start, entry_end, entry_strand = key.split(";")
-            entry_start = int(entry_start)
-            entry_end = int(entry_end)
+
+            first_entry_start, first_entry_end, first_entry_strand,\
+                second_entry_start, second_entry_end, second_entry_strand = key.split(";")
+
+            first_entry_start = int(first_entry_start)
+            first_entry_end = int(first_entry_end)
+            second_entry_start = int(second_entry_start)
+            second_entry_end = int(second_entry_end)
 
             #print entry_start, entry_end, entry_strand
 
-            # We only care about matches on the same strand
-            if (strand != entry_strand):
-                continue
+            strand_values = []
 
-            if ((start >= entry_start and start <= entry_end) or
-                (end >= entry_start and end <= entry_end) or
-                (entry_start >= start and entry_start <= end) or
-                (entry_end >= start and entry_end <= end)):
-                return (True, value[Table.UNIQUE_ID_FIELD])
+            if (strand_val == None):
+                strand_values.append(Table.POS_STRAND)
+                strand_values.append(Table.NEG_STRAND)
+            else:
+                strand_values.append(strand_val)
 
-            distance = min(abs(start - entry_start),
-                           abs(end - entry_end),
-                           abs(start - entry_end),
-                           abs(end - entry_start))
+            # in case we don't sure of the strand check for both
+            for strand in strand_values:
 
-            if (closest_segment == None):
-                closest_segment = value[Table.UNIQUE_ID_FIELD]
-                closest_segment_distance = distance
+                # We only care about matches on the same strand
+                if (strand != first_entry_strand and strand != second_entry_strand):
+                    continue
 
-            elif (distance < closest_segment_distance):
-                closest_segment = value[Table.UNIQUE_ID_FIELD]
-                closest_segment_distance = distance
+                if ((start >= first_entry_start and start <= first_entry_end) or
+                    (end >= first_entry_start and end <= first_entry_end) or
+                    (first_entry_start >= start and first_entry_start <= end) or
+                    (first_entry_end >= start and first_entry_end <= end)):
 
+                    matches.append((True, value[Table.UNIQUE_ID_FIELD]))
 
-        return (False, closest_segment)
+                elif ((start >= second_entry_start and start <= second_entry_end) or
+                    (end >= second_entry_start and end <= second_entry_end) or
+                    (second_entry_start >= start and second_entry_start <= end) or
+                    (second_entry_end >= start and second_entry_end <= end)):
+
+                    matches.append((True, value[Table.UNIQUE_ID_FIELD]))
+
+                first_distance = min(abs(start - first_entry_start),
+                                     abs(end - first_entry_end),
+                                     abs(start - first_entry_end),
+                                     abs(end - first_entry_start))
+
+                second_distance = min(abs(start - second_entry_start),
+                                      abs(end - second_entry_end),
+                                      abs(start - second_entry_end),
+                                      abs(end - second_entry_start))
+
+                distance = min(first_distance, second_distance)
+
+                if (closest_segment == None):
+                    closest_segment = value[Table.UNIQUE_ID_FIELD]
+                    closest_segment_distance = distance
+
+                elif (distance < closest_segment_distance):
+                    closest_segment = value[Table.UNIQUE_ID_FIELD]
+                    closest_segment_distance = distance
+
+        if (len(matches) == 0):
+
+            matches.append((False, closest_segment))
+
+        return matches
+
+    def findByField(self, field_name, search_value):
+        for key, value in self._dctData.items():
+
+            compared = value[field_name]
+
+            if (type(search_value) is str):
+                search_value = search_value.lower()
+                compared = value[field_name].lower()
+
+            if (compared == search_value):
+                return (key, value)
+
+        return (None, None)
 
     def findById(self, id):
+        return self.findByField(Table.UNIQUE_ID_FIELD, id)
 
+    def findByName(self, name):
+        return self.findByField(Table.NAME_FIELD, name)
+
+    def findByOtherNames(self, name):
         for key, value in self._dctData.items():
-            if (value[Table.UNIQUE_ID_FIELD] == id):
+            if (name in value[Table.OTHER_NAMES_FIELD]):
                 return (key, value)
 
         return (None, None)
 
 if __name__ == "__main__":
 
-    print min(3, 2)
     table = Table("Known sRNA")
 
     dct = {"name" : "test1",
            "name2" : "test2"}
 
-    table.insert(10, 100, "positive", **dct)
-    table.insert(6, 9, "positive", **dct)
+    table.insert((10, 12, "+"), (13, 100, "+"), **dct)
+    table.insert((6, 9, "+"), (6, 9, "+"), **dct)
 
     print table._dctData
 
-    print table.is_overlaps(1, 7, "positive")
+    print table.is_overlaps(1, 7, "+")
--- table_builder/TableLoader.py
+++ table_builder/TableLoader.py
@@ -1,12 +1,19 @@
+from Bio.TogoWS import entry
+
 __author__ = 'amirbar'
 
 from Table import Table
 
 class TableLoader(object):
 
-    START_BASE_KEY = "from"
-    END_BASE_KEY = "to"
-    STRAND_KEY = "strand"
+    FIRST_START_BASE_KEY = "start_1"
+    FIRST_END_BASE_KEY = "end_1"
+    SECOND_START_BASE_KEY = "start_2"
+    SECOND_END_BASE_KEY = "end_2"
+    FIRST_STRAND_KEY = "strand_1"
+    SECOND_STRAND_KEY = "strand_2"
+    STRAND_POSITIVE = "+"
+    STRAND_NEGATIVE = "-"
 
     def load(self, path):
         raise NotImplementedError
@@ -16,10 +23,16 @@ class TableLoader(object):
 
         for row in row_list:
 
-            result.insert(row.pop(TableLoader.START_BASE_KEY),
-                          row.pop(TableLoader.END_BASE_KEY),
-                          row.pop(TableLoader.STRAND_KEY),
-                          **row)
+            first = (row.pop(TableLoader.FIRST_START_BASE_KEY),
+                     row.pop(TableLoader.FIRST_END_BASE_KEY),
+                     row.pop(TableLoader.FIRST_STRAND_KEY))
+
+            second = (row.pop(TableLoader.SECOND_START_BASE_KEY),
+                     row.pop(TableLoader.SECOND_END_BASE_KEY),
+                     row.pop(TableLoader.SECOND_STRAND_KEY))
+
+
+            result.insert(first, second, **row)
 
         return result
 
@@ -39,11 +52,20 @@ class PDFTableLoader(TableLoader):
             dct = {}
 
             for key, val in zip(name_list, value_list):
-                dct[key] = val
+                dct[key.replace("\n", "")] = val.replace("\n", "")
 
             result.append(dct)
 
         fl.close()
+
+        for entry in result:
+
+            entry[TableLoader.FIRST_STRAND_KEY] = None
+            entry[TableLoader.SECOND_STRAND_KEY] = None
+            entry[TableLoader.FIRST_START_BASE_KEY] = entry.pop('right_end')
+            entry[TableLoader.FIRST_END_BASE_KEY] = entry.pop('left_end')
+            entry[TableLoader.SECOND_START_BASE_KEY] = entry[TableLoader.FIRST_START_BASE_KEY]
+            entry[TableLoader.SECOND_END_BASE_KEY] = entry[TableLoader.FIRST_END_BASE_KEY]
 
         return result
 
@@ -52,7 +74,7 @@ class GeneTableLoader(TableLoader):
 
         ignore = ["REPLICON",
                   "SWISS-PROT-ID",
-                  "GENE-CLASS\n",
+                  "GENE-CLASS",
                   "PRODUCT-NAME",
                   "GENE-CLASS"]
 
@@ -76,44 +98,42 @@ class GeneTableLoader(TableLoader):
                 elif (key in ignore):
                     continue
                 else:
-                    dct[key] = val
+                    dct[key.replace("\n", "")] = val.replace("\n", "")
 
             result.append(dct)
 
         fl.close()
 
         for entry in result:
-            if (int(entry['START-BASE']) < int(entry['END-BASE'])):
-                entry["strand"] = "F"
-            else:
-                entry["strand"] = "R"
 
-            entry[TableLoader.START_BASE_KEY] = entry.pop('START-BASE')
-            entry[TableLoader.END_BASE_KEY] = entry.pop('END-BASE')
+            if (int(entry['START-BASE']) < int(entry['END-BASE'])):
+                entry[TableLoader.FIRST_STRAND_KEY] = TableLoader.STRAND_POSITIVE
+                entry[TableLoader.SECOND_STRAND_KEY] = TableLoader.STRAND_POSITIVE
+            else:
+                entry[TableLoader.FIRST_STRAND_KEY] = TableLoader.STRAND_NEGATIVE
+                entry[TableLoader.SECOND_STRAND_KEY] = TableLoader.STRAND_NEGATIVE
+
+            entry[TableLoader.FIRST_START_BASE_KEY] = entry.pop('START-BASE')
+            entry[TableLoader.FIRST_END_BASE_KEY] = entry.pop('END-BASE')
+            entry[TableLoader.SECOND_START_BASE_KEY] = entry[TableLoader.FIRST_START_BASE_KEY]
+            entry[TableLoader.SECOND_END_BASE_KEY] = entry[TableLoader.FIRST_END_BASE_KEY]
 
 
         return result
 
 class OurTableLoader(TableLoader):
     def load(self, path):
-        ignore = ["RNA1 description",
-                  "RNA2 description",
-                  "odds ratio",
-                  "total other interactions",
-                  "interactions",
-                  "other interactions of RNA1",
-                  "other interactinos of RNA2",
-                  "Fisher's exact test p-value"]
+        ignore = []
 
         result = []
 
         fl = open(path, "rb")
 
-        dct = {}
-
         name_list = fl.readline().split("\t")
 
         for line in fl.readlines():
+
+            dct = {}
 
             value_list = line.split("\t")
 
@@ -121,11 +141,19 @@ class OurTableLoader(TableLoader):
                 if (key in ignore):
                     continue
                 else:
-                    dct[key] = val
+                    dct[key.replace("\n", "")] = val.replace("\n", "")
 
             result.append(dct)
 
         fl.close()
+
+        for entry in result:
+            entry[TableLoader.FIRST_START_BASE_KEY] = entry.pop('RNA1 from')
+            entry[TableLoader.SECOND_START_BASE_KEY] = entry.pop('RNA2 from')
+            entry[TableLoader.FIRST_END_BASE_KEY] = entry.pop('RNA1 to')
+            entry[TableLoader.SECOND_END_BASE_KEY] = entry.pop('RNA2 to')
+            entry[TableLoader.FIRST_STRAND_KEY] = entry.pop('RNA1 strand')
+            entry[TableLoader.SECOND_STRAND_KEY] = entry.pop('RNA2 strand')
 
 
         return result
@@ -133,13 +161,158 @@ class OurTableLoader(TableLoader):
 if (__name__ == "__main__"):
 
 
+
+    # loader = OurTableLoader()
+    # result = loader.load("./our_files/assign-type-to-all-chimeras-of-Iron_limitation_CL_FLAG207_208_305_all_fragments_l25.txt_all_interactions.with-type")
+    #
+    # j = 0
+    # for i in result:
+    #    if j == 10:
+    #        break
+    #    print i
+    #    j += 1
+    #
+    #
+    # table = loader.createTable("chimera", result)
+    #
+    #
+    # matches = table.is_overlaps(2348425, 2348445, "+")
+    #
+    # for match in matches:
+    #     res, id = match
+    #     print res, table.findById(id)
+
+
+
     loader = GeneTableLoader()
     result = loader.load("genes.col")
-    #result = TableLoader().load_our_tables("./our_files/assign-type-to-all-chimeras-of-Iron_limitation_CL_FLAG207_208_305_all_fragments_l25.txt_all_interactions.with-type")
-
 
     table = loader.createTable("geneDB", result)
 
+    count = {}
 
-    res, id = table.is_overlaps(510860, 510866, "F")
-    print res, table.findById(id)
+    for entry in result:
+
+        name = entry["NAME"].lower()
+
+        if not count.has_key(name):
+            count[name] = 1
+        else:
+            count[name] += 1
+
+        if count[name] > 1:
+            print "warning"
+
+    old_count = {}
+
+    warnings = ""
+    old_warnings = ""
+
+    for entry in result:
+
+        for name in entry["other_names"]:
+
+            lower_name = name.lower()
+
+            if count.has_key(lower_name):
+                count[lower_name] += 1
+                warnings += ("warning: %s is an old name of %s = %d\n" % (lower_name, entry["NAME"], count[lower_name]))
+            elif (old_count.has_key(lower_name)):
+                old_count[lower_name] += 1
+                old_warnings += ("warning: old multiplicity for %s = %d\n" % (lower_name, old_count[lower_name]))
+            else:
+                old_count[lower_name] = 1
+
+    print "-" * 100
+    print warnings
+    print "-" * 100
+    print old_warnings
+    print "-" * 100
+
+    # matches = table.is_overlaps(510860, 510863, "+")
+    #
+    # for match in matches:
+    #     res, id = match
+    #     print res, table.findById(id)
+    #
+    # print table.findByName("ybaS")
+
+    pdfLoader = PDFTableLoader()
+
+    result = pdfLoader.load("./parsed_files/s7.table")
+    pdfTable = pdfLoader.createTable("s5", result)
+
+    print "-" * 100
+    print "-" * 100
+    print "-" * 100
+
+
+    other_name_matches = ""
+
+    total_count = {}
+    total_count.update(count)
+    total_count.update(old_count)
+
+
+    # table_name = "sRNA-name"
+    table_name = "name"
+
+    # find matching candidates to copy their directionality
+    for entry in result:
+
+
+        match = table.findByField("NAME", entry[table_name])
+
+
+        if (match != (None, None)):
+
+            first_entry_start, first_entry_end, dummyA,\
+                second_entry_start, second_entry_end, dummyB = \
+                pdfTable.findByField(table_name, entry[table_name])[0].split(";")
+
+            startDiff = int(first_entry_start)
+            endDiff = int(first_entry_end)
+
+            first_entry_start, first_entry_end, dummyA,\
+                second_entry_start, second_entry_end, dummyB = match[0].split(";")
+
+            startDiff -= int (int(first_entry_start))
+            endDiff -= int(first_entry_end)
+
+            print "%s from PDF match to: %s diff is: %d;%d" % (entry[table_name], match[0], startDiff, endDiff)
+
+            if(total_count[entry[table_name].lower()] > 1):
+                print "%s has multiple instances" % entry[table_name]
+        else:
+
+            match = table.findByOtherNames(entry[table_name])
+
+            other_name_matches += "%s match to other names: %s\n" % (entry[table_name], match[0])
+
+            if (match[0] != None):
+                first_entry_start, first_entry_end, dummyA,\
+                    second_entry_start, second_entry_end, dummyB = \
+                    pdfTable.findByField(table_name, entry[table_name])[0].split(";")
+
+                startDiff = int(first_entry_start)
+                endDiff = int(first_entry_end)
+
+                first_entry_start, first_entry_end, dummyA,\
+                    second_entry_start, second_entry_end, dummyB = match[0].split(";")
+
+                startDiff -= int (int(first_entry_start))
+                endDiff -= int(first_entry_end)
+
+                other_name_matches += "the diff is %d;%d\n" % (startDiff, endDiff)
+
+            if (total_count.has_key(entry[table_name])):
+
+                other_name_matches += "%s appears %d in countings\n" % (entry[table_name], total_count[entry[table_name]])
+
+    print "-" * 100
+    print other_name_matches
+
+    # result = PDFTableLoader().load("./parsed_files/s5.table")
+    #
+    # for entry in result:
+    #     print entry

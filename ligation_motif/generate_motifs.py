cc130083408f65d03d02a0c23e30202955c87c18
Amir Bar <amirbar@emerald.hanah>
2016-04-04 15:42:57 +0300

    Now has an option to limit the amount of bases around ligation point to show in diagrams

--- ligation_motif/generate_motifs.py
+++ ligation_motif/generate_motifs.py
@@ -13,240 +13,258 @@ GAP = "-"
 ALPHABET = Gapped(IUPAC.unambiguous_dna)
 
 def process_command_line(argv):
-    """
-    Return a 2-tuple: (settings object, args list).
-    `argv` is a list of arguments, or `None` for ``sys.argv[1:]``.
-    """
-    if argv is None:
-        argv = sys.argv[1:]
+	"""
+	Return a 2-tuple: (settings object, args list).
+	`argv` is a list of arguments, or `None` for ``sys.argv[1:]``.
+	"""
+	if argv is None:
+		argv = sys.argv[1:]
 
-    # initialize the parser object:
-    parser = optparse.OptionParser(
-        formatter=optparse.TitledHelpFormatter(width=100),
-        add_help_option=None)
+	# initialize the parser object:
+	parser = optparse.OptionParser(
+		formatter=optparse.TitledHelpFormatter(width=100),
+		add_help_option=None)
 
-    parser.add_option(
-        "-r", "--reads_fusion",
-        help="the output file from Asaf's script 'detect_fusion_point.py'")
+	parser.add_option(
+		"-r", "--reads_fusion",
+		help="the output file from Asaf's script 'detect_fusion_point.py'")
 
-    parser.add_option(
-        "-l", "--logo_file",
-        help="file name for the output logo file")
+	parser.add_option(
+		"-l", "--logo_file",
+		help="file name for the output logo file")
 
-    parser.add_option(
-        "-s", "--score_diagram",
-        help="file name for the output score diagram")
+	parser.add_option(
+		"-s", "--score_diagram",
+		help="file name for the output score diagram")
 
-    parser.add_option(
-        "-c", "--count_diagram",
-        help="file name for the output count diagram")
+	parser.add_option(
+		"-c", "--count_diagram",
+		help="file name for the output count diagram")
 
-    parser.add_option(      # customized description; put --help last
-        "-h", "--help", action="help",
-        help="Show this help message and exit.")
+	parser.add_option(
+		"-d", "--distance",
+		help="maximal distance from ligation point", default=10, type="int")
 
-    settings, args = parser.parse_args(argv)
+	parser.add_option(	  # customized description; put --help last
+		"-h", "--help", action="help",
+		help="Show this help message and exit.")
 
-    # check number of arguments, verify values, etc.:
-    if args:
-        parser.error('program takes no command-line arguments; '
-                     '"%s" ignored.' % (args,))
+	settings, args = parser.parse_args(argv)
 
-    # further process settings & args if necessary
+	# check number of arguments, verify values, etc.:
+	if args:
+		parser.error('program takes no command-line arguments; '
+					 '"%s" ignored.' % (args,))
 
-    return settings, args
+	# further process settings & args if necessary
+
+	return settings, args
 
 def format_input(chimera_frags_list):
 
-    result = []
+	result = []
 
-    for chimera in chimera_frags_list:
-        first_frag, second_frag = chimera
-        result.append((Seq(first_frag, ALPHABET), Seq(second_frag, ALPHABET)))
+	for chimera in chimera_frags_list:
+		first_frag, second_frag = chimera
+		result.append((Seq(first_frag, ALPHABET), Seq(second_frag, ALPHABET)))
 
-    return  result
+	return  result
 
 
 def format_file_input(path_to_file):
 
-    FRAG_1_SEQ_INDEX = 6
-    FRAG_2_SEQ_INDEX = 8
-    FRAG_1_SCORE_START = 9
+	FRAG_1_SEQ_INDEX = 6
+	FRAG_2_SEQ_INDEX = 8
+	FRAG_1_SCORE_START = 9
 
-    fragment_as_seq_list = []
-    positive_pos_score_list = []
-    negative_pos_score_list = []
+	fragment_as_seq_list = []
+	positive_pos_score_list = []
+	negative_pos_score_list = []
 
-    with open(path_to_file, "rb") as fl:
+	with open(path_to_file, "rb") as fl:
 
-        for line in fl:
-            row = line.replace("\n", "").split("\t")
-            FRAG_2_SCORE_START = FRAG_1_SCORE_START + len(row[FRAG_1_SEQ_INDEX])
+		for line in fl:
+			row = line.replace("\n", "").split("\t")
+			FRAG_2_SCORE_START = FRAG_1_SCORE_START + len(row[FRAG_1_SEQ_INDEX])
 
-            if len(row[FRAG_1_SEQ_INDEX]) + len(row[FRAG_2_SEQ_INDEX]) != len(row[FRAG_1_SCORE_START:]):
-                print "-" * 100
-                print "skipped row"
-                print row[FRAG_1_SEQ_INDEX]
-                print row[FRAG_2_SEQ_INDEX]
-                print "-" * 100
-                continue
+			if len(row[FRAG_1_SEQ_INDEX]) + len(row[FRAG_2_SEQ_INDEX]) != len(row[FRAG_1_SCORE_START:]):
+				print "-" * 100
+				print "skipped row"
+				print row[FRAG_1_SEQ_INDEX]
+				print row[FRAG_2_SEQ_INDEX]
+				print "-" * 100
+				continue
 
-            # Get the fragments
-            fragment_as_seq_list.append((Seq(row[FRAG_1_SEQ_INDEX], ALPHABET), Seq(row[FRAG_2_SEQ_INDEX], ALPHABET)))
+			# Get the fragments
+			fragment_as_seq_list.append((Seq(row[FRAG_1_SEQ_INDEX], ALPHABET), Seq(row[FRAG_2_SEQ_INDEX], ALPHABET)))
 
-            # Get negative scores
-            # print "*" * 50
-            # print "row length:", len(row)
-            # print "start-end:", FRAG_1_SCORE_START, FRAG_2_SCORE_START - 1
-            score = [float(row[index]) for index in range(FRAG_2_SCORE_START - 1, FRAG_1_SCORE_START - 1, -1)]
-            negative_pos_score_list.append(score)
+			# Get negative scores
+			# print "*" * 50
+			# print "row length:", len(row)
+			# print "start-end:", FRAG_1_SCORE_START, FRAG_2_SCORE_START - 1
+			score = [float(row[index]) for index in range(FRAG_2_SCORE_START - 1, FRAG_1_SCORE_START - 1, -1)]
+			negative_pos_score_list.append(score)
 
-            # Get positive scores
-            # print FRAG_2_SCORE_START, len(row[FRAG_2_SEQ_INDEX]), len(row)
-            score = [float(row[index]) for index in range(FRAG_2_SCORE_START, FRAG_2_SCORE_START + len(row[FRAG_2_SEQ_INDEX]))]
+			# Get positive scores
+			# print FRAG_2_SCORE_START, len(row[FRAG_2_SEQ_INDEX]), len(row)
+			score = [float(row[index]) for index in range(FRAG_2_SCORE_START, FRAG_2_SCORE_START + len(row[FRAG_2_SEQ_INDEX]))]
 
-            positive_pos_score_list.append(score)
+			positive_pos_score_list.append(score)
 
 
-    return fragment_as_seq_list, negative_pos_score_list, positive_pos_score_list
+	return fragment_as_seq_list, negative_pos_score_list, positive_pos_score_list
 
 
 def get_max_len_in_chimera(fragment_as_seq_list):
 
-    first_frag_list, second_frag_list = zip(*fragment_as_seq_list)
+	first_frag_list, second_frag_list = zip(*fragment_as_seq_list)
 
-    return len(max(first_frag_list, key=len)), len(max(second_frag_list, key=len))
+	return len(max(first_frag_list, key=len)), len(max(second_frag_list, key=len))
 
 
 def pad_sequences(chimera_frags_list):
 
-    max_first, max_second = get_max_len_in_chimera(chimera_frags_list)
+	max_first, max_second = get_max_len_in_chimera(chimera_frags_list)
 
-    result = []
+	result = []
 
-    for chimera in chimera_frags_list:
-        first_frag, second_frag = chimera
+	for chimera in chimera_frags_list:
+		first_frag, second_frag = chimera
 
-        first_frag = (max_first - len(first_frag)) * GAP + first_frag
-        second_frag += (max_second - len(second_frag)) * GAP
+		first_frag = (max_first - len(first_frag)) * GAP + first_frag
+		second_frag += (max_second - len(second_frag)) * GAP
 
-        result.append((first_frag, second_frag))
+		result.append((first_frag, second_frag))
 
-    return result
+	return result
 
 
 def merge_frgaments(padded_fragments_list):
 
-    result = []
+	result = []
 
-    for chimera in padded_fragments_list:
-        first_frag, second_frag = chimera
-        result.append(Seq(str(first_frag + second_frag), ALPHABET))
+	for chimera in padded_fragments_list:
+		first_frag, second_frag = chimera
+		result.append(Seq(str(first_frag + second_frag), ALPHABET))
 
-    return result
+	return result
 
 
-def create_sequnce_file(filename, sequence_list):
+def create_sequnce_file(filename, sequence_list, lower_bound, upper_bound, ligation_index):
 
-    with open(filename, "wb") as fl:
+	with open(filename, "wb") as fl:
 
-        for sequence in sequence_list:
-            fl.write(">\n")
-            fl.write(str(sequence) + "\n")
+		for sequence in sequence_list:
+			fl.write(">\n")
+			fl.write(str(sequence)[ligation_index + lower_bound:ligation_index] + \
+					 "-" + \
+					 str(sequence)[ligation_index:ligation_index+upper_bound + 1] + \
+					 "\n")
 
 
 def get_position_statistics(fragment_as_seq_list, negative_pos_score_list, positive_pos_score_list):
 
-    max_first, max_second = get_max_len_in_chimera(fragment_as_seq_list)
+	max_first, max_second = get_max_len_in_chimera(fragment_as_seq_list)
 
-    pos_sum_dct = {index: 0 for index in range(-max_first, 0)}
-    pos_sum_dct.update({index: 0 for index in range(1, max_second + 1)})
-    pos_count_dct = {index: 0 for index in range(-max_first, 0)}
-    pos_count_dct.update({index: 0 for index in range(1, max_second + 1)})
+	pos_sum_dct = {index: 0 for index in range(-max_first, 0)}
+	pos_sum_dct.update({index: 0 for index in range(1, max_second + 1)})
+	pos_count_dct = {index: 0 for index in range(-max_first, 0)}
+	pos_count_dct.update({index: 0 for index in range(1, max_second + 1)})
 
-    # Sum over the negatives
-    for score_list in negative_pos_score_list:
-        for index, score in enumerate(score_list):
+	# Sum over the negatives
+	for score_list in negative_pos_score_list:
+		for index, score in enumerate(score_list):
 
-            pos_sum_dct[-(index + 1)] += score
-            pos_count_dct[-(index + 1)] += 1
+			pos_sum_dct[-(index + 1)] += score
+			pos_count_dct[-(index + 1)] += 1
 
-    # Sum score over the positives
-    for score_list in positive_pos_score_list:
-        for index, score in enumerate(score_list):
+	# Sum score over the positives
+	for score_list in positive_pos_score_list:
+		for index, score in enumerate(score_list):
 
-            pos_sum_dct[index + 1] += score
-            pos_count_dct[index + 1] += 1
+			pos_sum_dct[index + 1] += score
+			pos_count_dct[index + 1] += 1
 
-    return pos_sum_dct, pos_count_dct
+	return pos_sum_dct, pos_count_dct
 
 
 def print_settings(settings):
 
-    print "-" * 100
-    print "| settings:"
-    print "-" * 100
+	print "-" * 100
+	print "| settings:"
+	print "-" * 100
 
-    for key, value in settings.items():
-        print "| %s = %s" % (key, value)
+	for key, value in settings.items():
+		print "| %s = %s" % (key, value)
 
-    print "-" * 100
+	print "-" * 100
 
 def run(argv=None):
 
-    settings, args = process_command_line(None)
+	settings, args = process_command_line(None)
 
-    fragment_as_seq_list, negative_pos_score_list, positive_pos_score_list = \
-        format_file_input(settings.reads_fusion)
+	fragment_as_seq_list, negative_pos_score_list, positive_pos_score_list = \
+		format_file_input(settings.reads_fusion)
 
-    if settings.logo_file != None:
-        padded_fragments_list = pad_sequences(fragment_as_seq_list)
+	# Score diagram
+	pos_sum_dct, pos_count_dct = get_position_statistics(fragment_as_seq_list,
+														 negative_pos_score_list,
+														 positive_pos_score_list)
 
-        merged_fragments_list = merge_frgaments(padded_fragments_list)
+	stats_dict = {position: float(pos_sum_dct[position]) / float(pos_count_dct[position])
+				  for position in pos_sum_dct.keys()}
 
-        chimera_motifs = motifs.create(merged_fragments_list, ALPHABET)
 
-        print chimera_motifs
-        print chimera_motifs.counts
+	ligation_index = abs(min(stats_dict.keys()))
+	lower_bound = min(stats_dict.keys())
+	upper_bound = max(stats_dict.keys())
 
-        create_sequnce_file("logo_data", merged_fragments_list)
+	if settings.distance:
+		lower_bound = max(lower_bound, -settings.distance)
+		upper_bound = min(upper_bound, settings.distance)
 
-        os.system("/home/users/amirbar/.local/bin/weblogo -A dna -c classic -s large --format PNG < logo_data  > %(logo_file)s" % \
-                  {"logo_file": settings.logo_file})
+	# Generate average base score plot
+	points = [[i, stats_dict[i]] for i in range (lower_bound, 0)] + \
+			 [[i, stats_dict[i]] for i in range (1, upper_bound + 1)]
 
-    if settings.score_diagram != None:
-        pos_sum_dct, pos_count_dct = get_position_statistics(fragment_as_seq_list,
-                                                             negative_pos_score_list,
-                                                             positive_pos_score_list)
+	plt.xlim(lower_bound - 1, upper_bound + 1)
+	plt.ylim(0, 1)
+	plt.hold(True)
 
-        stats_dict = {position: float(pos_sum_dct[position]) / float(pos_count_dct[position])
-                      for position in pos_sum_dct.keys()}
+	for pt in points:
+		plt.plot([pt[0], pt[0]], [0,pt[1]], "b")
 
-        # Generate average base score plot
-        points = [[i, stats_dict[i]] for i in range (min(stats_dict.keys()), 0)] + \
-                 [[i, stats_dict[i]] for i in range (1, max(stats_dict.keys()) + 1)]
+	if settings.score_diagram != None:
+		plt.savefig(settings.score_diagram)
 
-        plt.xlim(min(stats_dict.keys()) - 1, max(stats_dict.keys()) + 1)
-        plt.ylim(0, 1)
-        plt.hold(True)
+	# Generate count per position plot
+	points = [[i, pos_count_dct[i]] for i in range (lower_bound, 0)] + \
+			 [[i, pos_count_dct[i]] for i in range (1, upper_bound + 1)]
 
-        for pt in points:
-            plt.plot([pt[0], pt[0]], [0,pt[1]], "b")
+	plt.xlim(lower_bound - 1, upper_bound + 1)
+	plt.ylim(0, max(pos_count_dct.values()))
+	plt.hold(True)
 
-        plt.savefig(settings.score_diagram)
+	for pt in points:
+		plt.plot([pt[0], pt[0]], [0,pt[1]], "r")
 
-    if settings.count_diagram != None:
-        # Generate count per position plot
-        points = [[i, pos_count_dct[i]] for i in range (min(pos_count_dct.keys()), 0)] + \
-                 [[i, pos_count_dct[i]] for i in range (1, max(pos_count_dct.keys()) + 1)]
+	if settings.count_diagram != None:
+		plt.savefig(settings.count_diagram)
 
-        plt.xlim(min(pos_count_dct.keys()) - 1, max(pos_count_dct.keys()) + 1)
-        plt.ylim(0, max(pos_count_dct.values()))
-        plt.hold(True)
+	# Generate logo
+	if settings.logo_file != None:
+		padded_fragments_list = pad_sequences(fragment_as_seq_list)
 
-        for pt in points:
-            plt.plot([pt[0], pt[0]], [0,pt[1]], "r")
+		merged_fragments_list = merge_frgaments(padded_fragments_list)
 
-        plt.savefig(settings.count_diagram)
+		chimera_motifs = motifs.create(merged_fragments_list, ALPHABET)
 
+		print chimera_motifs
+		print chimera_motifs.counts
+
+		create_sequnce_file("logo_data", merged_fragments_list, lower_bound, upper_bound, ligation_index)
+
+		os.system("/home/users/amirbar/.local/bin/weblogo -A dna -c classic --resolution 600 --errorbars NO -i %(start_index)s --format PNG < logo_data  > %(logo_file)s" % \
+				  {"logo_file": settings.logo_file,
+				   "start_index": str(lower_bound)})
 run()
